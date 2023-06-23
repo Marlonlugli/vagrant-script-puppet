477fce64a7d693bfdb923d0f70d2b5bfd22100a0
Marlon Lugli <marlon.lugli@darede.com.br>
2023-06-23 18:42:31 -0300

    add files

---
+++ manifests/app.pp
@@ -0,0 +1,39 @@
+# Instalar o vim
+package { ["vim"]:
+ ensure => installed
+}
+
+# Instalar o curl
+package { ["curl"]:
+ ensure => installed
+}
+
+# Instalar o telnet
+package { ["telnet"]:
+ ensure => installed
+}
+
+# Instalar o unzip
+package { ["unzip"]:
+ ensure => installed
+}
+
+# Instalar o wget
+package { ["wget"]:
+ ensure => installed
+}
+
+# Instalar o net-tools
+package { ["net-tools"]:
+ ensure => installed
+}
+
+# Instalar o htop
+package { ["htop"]:
+ ensure => installed
+}
+
+# Instalar o nmap
+package { ["nmap"]:
+ ensure => installed
+}
